cc8011180ddd733b4af4dee296ca3047812b44da
MarcosMedeir0s <marcosmongin05@gmail.com>
2024-10-08 20:17:43 -0300

    Subindo atividade 8

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from fastapi import FastAPI, Form, Request
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from util import ler_html, salvar_contato, salvar_cadastro
+from models.produto_model import Produto
+from repositories import produto_repo
+from repositories.produto_repo import *
 import uvicorn
 
+criar_tabela()
 app = FastAPI()
 
 templates = Jinja2Templates(directory='templates')
@@ -27,7 +30,8 @@ def post_cadastro(
     estoque: str = Form(...),
     preco: str = Form(...),
     categoria: str = Form(...)):  # Adicionando categoria
-    salvar_cadastro(nome, descricao, estoque, preco, categoria)  # Corrigido para salvar_cadastro
+    produto = Produto(0, nome, descricao, estoque, preco, categoria)
+    produto = produto_repo.inserir(produto)
     return RedirectResponse('/cadastro_recebido', 303)
 
 @app.get("/cadastro_recebido")
@@ -35,4 +39,4 @@ def get_cadastro_recebido(request: Request):
     return templates.TemplateResponse('index.html', {"request": request})
 
 if __name__ == "__main__":
- uvicorn.run("main:app", port=8000, reload=True)
+    uvicorn.run("main:app", port=8000, reload=True)
